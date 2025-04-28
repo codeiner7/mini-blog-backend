4f68219e81004e1d5e7cf2408aec299703b9da0d
codeiner7 <rkeshri981@gmail.com>
2025-04-28 09:37:34 +0530

    deploy

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ connectDB();
 const app = express();
 app.use(json());
 app.use(cors({
-  origin: 'https://mini-blog-frontend-six.vercel.app'
+  origin: ["https://mini-blog-frontend-six.vercel.app"],
+    methods: ["GET", "POST"],
 }));
 
 
@@ -24,6 +25,10 @@ if (!fs.existsSync('uploads')) {
 app.use("/api/auth", authRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/blog", blogRoutes);
+
+app.get('/', (req, res) => {
+  res.json('Hello World');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
